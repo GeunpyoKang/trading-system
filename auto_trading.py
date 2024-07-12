4d2108b1d305068887bd39d69cb597228791a3df
sanghyung.kim <sh03.kim@samsung.com>
2024-07-12 14:49:13 +0900

    [Add] sell_nice_timing

--- auto_trading.py
+++ auto_trading.py
@@ -29,4 +29,11 @@ class AutoTrading:
             self.buy(ticker, current_price, price//current_price)
 
     def sell_nice_timing(self, ticker, shares):
-        pass
+        trends = []
+        for i in range(2):
+            trends.append(self.driver.get_price(ticker))
+            time.sleep(0.1)
+
+        if trends[0] > trends[1]:
+            current_price = self.driver.get_price(ticker)
+            self.sell(ticker, current_price, shares)
