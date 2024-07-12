67a8bcc077df308286d389aaa1c99a7b46228092
hodri <kanggeunpyo@gmail.com>
2024-07-12 15:32:13 +0900

    [Add] 자산관리 기능 테스트 코드

--- test_trading_system.py
+++ test_trading_system.py
@@ -37,7 +37,12 @@ class TestAutoTrading(TestCase):
 
     def test_buy_with_wrong_code(self):
         self.driver.buy.return_value = True
-        self.assertEqual(True, self.sut.buy(WRONG_CODE, 500, 10))
+        with self.assertRaises(AttributeError):
+            self.sut.buy(WRONG_CODE, 500, 10)
+
+    def test_buy_fail_cause_expensive(self):
+        self.driver.buy.return_value = True
+        self.assertEqual(False, self.sut.buy(get_random_code(), 1000001, 1))
 
     def test_sell(self):
         self.driver.sell.return_value = True
@@ -45,11 +50,15 @@ class TestAutoTrading(TestCase):
 
     def test_sell_with_wrong_code(self):
         self.driver.sell.return_value = True
-        self.assertEqual(True, self.sut.sell(WRONG_CODE, 700, 5))
+        with self.assertRaises(AttributeError):
+            self.sut.sell(WRONG_CODE, 700, 5)
+
+    def test_sell_fail_cause_dont_have(self):
+        self.driver.sell.return_value = True
+        self.assertEqual(False, self.sut.sell(get_random_code(), 1000, 1))
 
     def test_get_price(self):
         self.driver.get_price.return_value = 500
-
         self.assertEqual(500, self.sut.get_price('ABC123'))
 
     def test_buy_nice_timing(self):
@@ -63,3 +72,20 @@ class TestAutoTrading(TestCase):
         self.sut.sell_nice_timing('ABC123', 3)
         self.assertEqual(1, self.driver.sell.call_count)
         self.assertEqual(3, self.driver.get_price.call_count)
+
+    # 보유하고 있는 현금, 주식 환산액을 반환합니다.
+    def test_get_asset(self):
+        remain_money, stock_money = self.sut.get_asset()
+        self.assertEqual(1000000, remain_money)
+        self.assertEqual(0, stock_money)
+
+
+    # 종목 코드를 입력하면 그 주식의 갯수를 반환합니다.
+    def test_get_stock_quantity(self):
+        code1 = get_random_code()
+        code2 = get_random_code()
+        self.sut.buy(code1, 1000, 5)
+        self.sut.buy(code2, 500, 3)
+
+        self.assertEqual(5, self.sut.get_stock_quantity(code1))
+        self.assertEqual(3, self.sut.get_stock_quantity(code2))
