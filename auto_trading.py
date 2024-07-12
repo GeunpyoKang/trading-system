8074ce7db3cbbeb3b15347250770ea9140018a8a
hodri <kanggeunpyo@gmail.com>
2024-07-12 10:15:21 +0900

    [Add] AutoTrading class skeleton code

---
+++ auto_trading.py
@@ -0,0 +1,21 @@
+class AutoTrading:
+    def select_stock_brocker(self):
+        pass
+
+    def login(self, id, password):
+        pass
+
+    def buy(self, stock_code, price, quantity):
+        pass
+
+    def sell(self, stock_code, price, quantity):
+        pass
+
+    def get_price(self, stock_code):
+        pass
+
+    def buy_nice_timing(self, stock_code, amount_to_buy):
+        pass
+
+    def sell_nice_timing(self, stock_code, quantity):
+        pass
