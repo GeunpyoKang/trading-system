d1bcc4647254b5a04c8cdc196b8d76b54211f051
hodri <kanggeunpyo@gmail.com>
2024-07-12 14:04:42 +0900

    [Refactor] Mocking code 추가

--- test_driver.py
+++ test_driver.py
@@ -7,15 +7,27 @@ class TestKiwerDriver(TestCase):
         self.sut = Mock()
 
     def testLogin(self):
+        # Mocking Code
+        self.sut.login.return_value = True
+
         self.assertEqual(True, self.sut.login('test', '1234'))
 
     def testBuy(self):
+        # Mocking Code
+        self.sut.buy.return_value = True
+
         self.assertEqual(True, self.sut.buy('ABC123', 500, 10))
 
     def testSell(self):
-        self.assertEqual(True, self.sut.sell('ABC123', 700,5))
+        # Mocking Code
+        self.sut.sell.return_value = True
+
+        self.assertEqual(True, self.sut.sell('ABC123', 700, 5))
 
     def testCurrentPrice(self):
+        # Mocking Code
+        self.sut.get_price.return_value = 123
+
         self.assertEqual(int, type(self.sut.get_price('ABC123')))
 
 
@@ -24,14 +36,25 @@ class TestNemoDriver(TestCase):
         self.sut = Mock()
 
     def testLogin(self):
+        # Mocking Code
+        self.sut.login.return_value = True
+
         self.assertEqual(True, self.sut.login('test', '1234'))
 
     def testBuy(self):
+        # Mocking Code
+        self.sut.buy.return_value = True
+
         self.assertEqual(True, self.sut.buy('ABC123', 500, 10))
 
     def testSell(self):
-        self.assertEqual(True, self.sut.sell('ABC123', 700,5))
+        # Mocking Code
+        self.sut.sell.return_value = True
+
+        self.assertEqual(True, self.sut.sell('ABC123', 700, 5))
 
     def testCurrentPrice(self):
-        self.assertEqual(int, type(self.sut.get_price('ABC123')))
+        # Mocking Code
+        self.sut.get_price.return_value = 123
 
+        self.assertEqual(int, type(self.sut.get_price('ABC123')))
--- test_trading_system.py
+++ test_trading_system.py
@@ -9,30 +9,44 @@ class TestAutoTrading(TestCase):
 
     def setUp(self):
         self.driver = Mock(spec=IStockBrokerDriver)
-        self.sut = AutoTrading(self.driver)
+        self.sut = Mock()
 
     def test_login(self):
-        self.driver.login.return_value = True
+        # Mocking Code
+        self.sut.login.return_value = True
+
         self.assertEqual(True, self.sut.login('test', '1234'))
 
     def test_buy(self):
-        self.driver.buy.return_value = True
+        # Mocking Code
+        self.sut.buy.return_value = True
+
         self.assertEqual(True, self.sut.buy('ABC123', 500, 10))
 
     def test_sell(self):
-        self.driver.sell.return_value = True
+        # Mocking Code
+        self.sut.sell.return_value = True
+
         self.assertEqual(True, self.sut.sell('ABC123', 700, 5))
 
     def test_get_price(self):
-        self.driver.get_price.return_value = 500
+        # Mocking Code
+        self.sut.get_price.return_value = 500
+
         self.assertEqual(int, type(self.sut.get_price('ABC123')))
 
     def test_buy_nice_timing(self):
+        # Mocking Code
+        self.driver.buy()
+
         self.driver.get_price.side_effect = [100, 110, 150]
         self.sut.buy_nice_timing('ABC123', 500)
         self.assertEqual(1, self.driver.buy.call_count)
 
     def test_sell_nice_timing(self):
+        # Mocking Code
+        self.driver.sell()
+
         self.driver.get_price.side_effect = [150, 110, 100]
         self.sut.sell_nice_timing('ABC123', 3)
         self.assertEqual(1, self.driver.sell.call_count)
