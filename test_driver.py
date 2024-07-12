334a8ef51242a2d89b0582c3a02d61022b9cd4ab
sumin1s.song <sumin1s.song@samsung.com>
2024-07-12 15:41:11 +1000

    [feature/kiwerdriver] KiwerDriver test_driver 적용

--- test_driver.py
+++ test_driver.py
@@ -1,32 +1,33 @@
 from unittest import TestCase
 from unittest.mock import Mock
-
+from kiwer_driver import KiwerDriver
 
 class TestKiwerDriver(TestCase):
     def setUp(self):
-        self.sut = Mock()
+        # self.sut = Mock()
+        self.sut = KiwerDriver()
 
     def testLogin(self):
         # Mocking Code
-        self.sut.login.return_value = True
+        # self.sut.login.return_value = True
 
         self.assertEqual(True, self.sut.login('test', '1234'))
 
     def testBuy(self):
         # Mocking Code
-        self.sut.buy.return_value = True
+        # self.sut.buy.return_value = True
 
         self.assertEqual(True, self.sut.buy('ABC123', 500, 10))
 
     def testSell(self):
         # Mocking Code
-        self.sut.sell.return_value = True
+        # self.sut.sell.return_value = True
 
         self.assertEqual(True, self.sut.sell('ABC123', 700, 5))
 
     def testCurrentPrice(self):
         # Mocking Code
-        self.sut.get_price.return_value = 123
+        # self.sut.get_price.return_value = 123
 
         self.assertEqual(int, type(self.sut.get_price('ABC123')))
 
