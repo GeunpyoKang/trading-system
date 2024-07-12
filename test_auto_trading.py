f905c7c6f78f2f00c0729da2a2400e95949c8732
hodri <kanggeunpyo@gmail.com>
2024-07-12 10:51:58 +0900

    [Add] stock_broker 테스트 코드

---
+++ test_auto_trading.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from kiwer_api import KiwerAPI
+from nemo_api import NemoAPI
+
+
+class AutoTrading(TestCase):
+
+    def testSelectWrongStockBroker(self):
+        pass
+
+    def testSelectKiwerStockBroker(self):
+        pass
+
+    def testSelectNemoStockBroker(self):
+        pass
+
+    def testSelectMockStockBroker(self):
+        pass
+    #
+    # def testLoginWithInvalidID(self):
+    #     pass
+    #
+    # def testLoginWithInvalidPassword(self):
+    #     pass
+    #
+    # def testLoginWithValidPassword(self):
+    #     pass
+    #
+    # def testBuyWithInvalidParameter(self):
+    #     pass
+    #
+    # def testSuccessfulBuy(self):
+    #     pass
+    #
+    # def testFailBuy(self):
+    #     pass
